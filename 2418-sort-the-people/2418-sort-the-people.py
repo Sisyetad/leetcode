8ea8ae0b859864dc4b9bca3f33775c4bf7fe6ef1
Sisay Thaddeus <160520835+Sisyetad@users.noreply.github.com>
2024-12-01 22:27:13 +0300

    Time: 941 ms (5.08%), Space: 17.5 MB (6.55%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,10 +1,9 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        for i in range(len(heights)):
-            min_i=i
-            for j in range(i+1,len(heights)):
-                if heights[min_i]<heights[j]:
-                    min_i=j
-            heights[i],heights[min_i]=heights[min_i],heights[i]
-            names[i],names[min_i]=names[min_i],names[i]
+        n=len(heights)
+        for i in range(n):
+            for j in range(n-1):
+                if heights[j]<heights[j+1]:
+                    heights[j],heights[j+1]=heights[j+1],heights[j]
+                    names[j],names[j+1]=names[j+1],names[j]
         return names
